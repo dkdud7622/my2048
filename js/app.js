7100904fb995c5f35e36d67a46109bc92df775ce
unknown <dkdud7622@ajou.ac.kr>
2021-07-21 23:40:23 +0900

    updated

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-document.addEventListener("DOMcontentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const gridDisplay = document.querySelector(".grid");
     const scocreDisplay = document.querySelector("#score");
     const resultDisplay = document.querySelector("#result");
-    let width = num;
+    let width = 4;
+    console.log(width);
     let squares = [];
     let score = 0;
 
@@ -11,11 +12,13 @@ document.addEventListener("DOMcontentLoaded", () => {
     const FOUR = "four"
 
 
-    function createBoard(width) {
+    function createBoard() {
+        gridDisplay.style.display = "flex"
         for (let i = 0; i < width * width; i++) {
             square = document.createElement("div");
-            square.classlist.add(BLOCK_NONE);
+            square.classList.add(BLOCK_NONE);
             square.innerHTML = "";
+
             gridDisplay.appendChild(square);
             squares.push(square);
         }
@@ -29,20 +32,16 @@ document.addEventListener("DOMcontentLoaded", () => {
         if (squares[randomNumber].innerHTML === "") {
             let ran = Math.floor(Math.random() * 10)
             if (ran < 2) {
-                squares[randomNumber].classlist.remove(BLOCK_NONE);
-                squares[randomNumber].classlist.add(FOUR)
+                squares[randomNumber].classList.remove(BLOCK_NONE);
+                squares[randomNumber].classList.add(FOUR)
                 squares[randomNumber].innerHTML = 4;
             } else {
-                squares[randomNumber].classlist.remove(BLOCK_NONE);
-                squares[randomNumber].classlist.add(TWO)
+                squares[randomNumber].classList.remove(BLOCK_NONE);
+                squares[randomNumber].classList.add(TWO)
                 squares[randomNumber].innerHTML = 2;
             }
         } else {
             generate()
         }
     }
-
-
-
-
 })
